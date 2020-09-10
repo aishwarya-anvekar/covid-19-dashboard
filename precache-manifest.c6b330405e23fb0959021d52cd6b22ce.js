85ac66bca6faae20ac3165d8521d454bd993306f
aishwarya-anvekar <aishwaryaanvekar33@gmail.com>
2020-09-10 21:38:10 +0530

    Updates

---
+++ precache-manifest.c6b330405e23fb0959021d52cd6b22ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "350940accbcb055dafa63762050d7fe6",
+    "url": "/covid-19-dashboard/index.html"
+  },
+  {
+    "revision": "1547e4d6ab7b86d0c688",
+    "url": "/covid-19-dashboard/static/css/main.2da462f0.chunk.css"
+  },
+  {
+    "revision": "8a1b2b64feced150a91b",
+    "url": "/covid-19-dashboard/static/js/2.2b4f148b.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/covid-19-dashboard/static/js/2.2b4f148b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1547e4d6ab7b86d0c688",
+    "url": "/covid-19-dashboard/static/js/main.658498b9.chunk.js"
+  },
+  {
+    "revision": "0d5878ec4bbcb5c67f1a",
+    "url": "/covid-19-dashboard/static/js/runtime-main.8d77b089.js"
+  }
+]);
